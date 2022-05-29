c19f5df10a21f9799be6693078929712031bcfcf
John Kasun <johnkasun@live.com>
2022-05-29 07:02:02 -0400

    Add audio file opening error handling in exec

--- src/Exec/main.cpp
+++ src/Exec/main.cpp
@@ -19,11 +19,11 @@ int main(int argc, char* argv[])
 {
 	static const int fileBlockSize = 1023;
 
-	CAudioFileIf* audioFileIn = nullptr;
+	CAudioFileIf* audioFileIn  = nullptr;
 	CAudioFileIf* audioFileOut = nullptr;
 	CAudioFileIf::FileSpec_t fileSpec;
 
-	float** inputAudioData = nullptr;
+	float** inputAudioData  = nullptr;
 	float** outputAudioData = nullptr;
 	unique_ptr<Vibrato> vibrato = nullptr;
 
@@ -37,35 +37,50 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	inputFilePath = argv[1];
+	inputFilePath  = argv[1];
 	outputFilePath = argv[2];
-	widthInSec = atof(argv[3]);
-	freqInHz   = atof(argv[4]);
+	widthInSec	   = atof(argv[3]);
+	freqInHz       = atof(argv[4]);
 
 	// Open Audio Files
 	CAudioFileIf::create(audioFileIn);
-	CAudioFileIf::create(audioFileOut);
-
 	audioFileIn->openFile(inputFilePath, CAudioFileIf::FileIoType_t::kFileRead);
+	if (!audioFileIn->isOpen()) {
+		std::cout << "Couldn't open input file..." << std::endl;
+		CAudioFileIf::destroy(audioFileIn);
+		return -1;
+	}
 	audioFileIn->getFileSpec(fileSpec);
 
+	CAudioFileIf::create(audioFileOut);
 	audioFileOut->openFile(outputFilePath, CAudioFileIf::FileIoType_t::kFileWrite, &fileSpec);
-
-	// Allocate memory
-	inputAudioData = new float* [fileSpec.iNumChannels]{};
-	outputAudioData = new float* [fileSpec.iNumChannels]{};
-	for (int c = 0; c < fileSpec.iNumChannels; c++){
-		inputAudioData[c] = new float[fileBlockSize]{};
-		outputAudioData[c] = new float[fileBlockSize]{};
+	if (!audioFileOut->isOpen()) {
+		std::cout << "Couldn't open output file..." << std::endl;
+		CAudioFileIf::destroy(audioFileOut);
+		return -1;
 	}
 
 	// Create instance
 	vibrato.reset(new Vibrato());
 
-	// Initialize
-	vibrato->init(fileSpec.iNumChannels, fileSpec.fSampleRateInHz);
-	vibrato->setParam(Vibrato::Param_t::widthInSec, widthInSec);
-	vibrato->setParam(Vibrato::Param_t::freqInHz, freqInHz);
+	// Initialize vibrato
+	if (vibrato->init(fileSpec.iNumChannels, fileSpec.fSampleRateInHz) != Error_t::kNoError ||
+		vibrato->setParam(Vibrato::Param_t::widthInSec, widthInSec)	   != Error_t::kNoError ||
+		vibrato->setParam(Vibrato::Param_t::freqInHz, freqInHz)		   != Error_t::kNoError) {
+		std::cout << "Invalid parameters..." << std::endl;
+		CAudioFileIf::destroy(audioFileIn);
+		CAudioFileIf::destroy(audioFileOut);
+		vibrato.reset();
+		return -1;
+	}
+
+	// Allocate memory
+	inputAudioData  = new float* [fileSpec.iNumChannels]{};
+	outputAudioData = new float* [fileSpec.iNumChannels]{};
+	for (int c = 0; c < fileSpec.iNumChannels; c++){
+		inputAudioData[c]  = new float[fileBlockSize]{};
+		outputAudioData[c] = new float[fileBlockSize]{};
+	}
 
 	// Process
 	long long iNumFrames = fileBlockSize;
